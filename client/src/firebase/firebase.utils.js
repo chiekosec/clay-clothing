3251613e2114e526aab8197ea133e29968ad3a35
lakshit <lakshit75agarwal@gmail.com>
2022-03-05 21:35:13 +0530

    Fixed firebase and react versions

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const config = {
   apiKey: "AIzaSyCFoqTGVLz0aV2f-TW9aplPjzwAPf8Cg34",
